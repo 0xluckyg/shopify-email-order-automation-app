67d35dee44e35be88568338c1a4f21474c5a255c
EC2 Default User <scottsgcho@gmail.com>
2019-12-08 14:12:21 +0000

    package-lock commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 //Tells mongoose we're using bluebird promise
 mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, err => {
+    console.log('Failed to connect to MongoDB: ', err)
+});
 
 module.exports = {mongoose};
